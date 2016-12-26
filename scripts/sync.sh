2cb88b974987e86d9eb68cc3a6f9a5a675f33114
Jeffrey Slort <mail@jeffreyslort.nl>
2016-12-26 12:03:05 +0100

    updated rsync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -azP --delete --dry-run $1 pi@192.168.1.201:/home/pi/blackbox
+# uitvoeren in de root van het project!
+rsync -azP --delete . pi@192.168.1.200:/home/pi/blackbox
